d1e0d96b9cb74da5b9d8515cd064228c9743dbc4
kgajowni <kamil.gajowniczek@gmail.com>
2024-06-08 13:09:34 +0200

    first test

---
+++ test.py
@@ -0,0 +1,42 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from test_data import LoginData
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class Locators:
+    my_account_menu_item = (By.ID, 'menu-item-100')
+    username = (By.ID, 'username')
+    password = (By.ID, 'password')
+    login_button = (By.XPATH, '//*[@id="customer_login"]/div[1]/form/p[3]/button')
+    my_account_content = (By.XPATH, '//*[@class="woocommerce-MyAccount-content"]')
+
+
+def login(driver_instance):
+    WebDriverWait(driver_instance, 5).until(EC.visibility_of_element_located(Locators.my_account_menu_item))
+    driver_instance.find_element(*Locators.my_account_menu_item).click()
+    driver_instance.find_element(*Locators.username).send_keys(LoginData.correct_username)
+    driver_instance.find_element(*Locators.password).send_keys(LoginData.correct_password)
+    driver_instance.find_element(*Locators.login_button).click()
+
+
+def check_if_logged_in(driver_instance):
+    element = WebDriverWait(driver_instance, 5).until(EC.visibility_of_element_located(Locators.my_account_content))
+    return element.is_displayed()
+
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.get('https://tapsshop.pl/')
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test1_login(self):
+        login(self.driver)
+        self.assertTrue(check_if_logged_in(self.driver))
---
+++ test_data.py
@@ -0,0 +1,3 @@
+class LoginData:
+    correct_username = 'gorrion86@yopmail.com'
+    correct_password = 'v9XaB5UYc8pGy4Z'
